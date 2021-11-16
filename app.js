a88765b96b2eda39de02fe042544015971988c55
matiashidalgo11 <matiashidalgo18@gmail.com>
2021-11-16 17:32:29 -0300

    errores

--- app.js
+++ app.js
@@ -1,58 +1,40 @@
-import methodOverride from "method-override";
+/* import methodOverride from "method-override";
 import cors from "cors";
-import express from "express";
+import express from "express"; */
+const methodOverride = require("method-override");
+const cors = require("cors");
+const express = require("express");
 //
-
 const app = express();
 const log = console.log;
 
 let port = process.env.PORT || 3000;
 
-let users = ["bart", "lisa", "homero","marge"];
+let users = ["bart", "lisa", "homero", "marge"];
 
-app.use(cors());
+//app.use(cors());
 app.use(methodOverride());
-app.use(express.urlencoded({extendes:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.get("/users", (req, res) => {
-    res.send(users);
+   res.send(users);
 });
 
-app.post("/users/create/:nombre", (req, res) => {
-
-    users.push(req.params.nombre);
-    res.send("Usuario Enviado");
-
+app.post("/user/create/", (req, res) => {
+   users.push(req.body.nombre);
+   res.send("usuario creado");
 });
 
-app.put("/users/modificar/:nombre/:nuevoNombre", (req, res) =>{
-
-    let nombre = req.params.nombre;
-
-    let index = users.findIndex((element) => element === nombre);
-
-    users[index] = req.params.nuevoNombre;
-
-    res.send(`El usuario ${nombre} ahora se llama ${users[index]}`);
-    
+app.delete("/user/delete/:nombre", (req, res) => {
+   users.filter(el => el != req.params.nombre)
+   res.send("usuario borrado");
 });
-
-app.delete("/users/delete/:nombre" , (req, res) => {
-    
-    let nombre = req.params.nombre;
-
-  /*   let index = users.findIndex((element) => element === nombre);
-    users.slice(index,1); */
-
-    users = users.filter(user => user !== nombre);
-
-    res.send(`El usuario ${nombre} fue borrado`);
-
-});
-
+app.put("/user/modify/:nombre/:nombre2", (req, res) => {
+   users = users.map(el => (req.params.nombre === el) ? req.params.nombre2 : el)
+   res.send("usuario ha sido modificado");
+})
 
 app.listen(port, () => {
-    log("star server");
+   log("start server");
 });
